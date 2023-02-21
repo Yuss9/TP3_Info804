6c62a19adb0ec2d0a26d4b68ed2acfa4ed9c34c3
Paul Delifer <paul.delifer@ikmail.com>
2023-02-21 09:45:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+
+//serve document with express
+
+const express = require('express');
+const app = express();
+const http = require("http").Server(app);
+
+const port = 5000;
+
+var comptes = new Map();
+
+app.use(express.static(__dirname, ""));
+
+app.get('/q1', function (req, res) {
+    res.sendFile(__dirname + "/question-1.html");
+});
+app.get('/q2',function (req,res){
+    res.sendFile(__dirname + "/question-2.html");
+})
+
+http.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+});
+
+
+
+
